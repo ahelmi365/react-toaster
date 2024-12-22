7e4e28c9f39cd4fe1a499d71d187fc9191c917a9
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 11:27:36 +0200

    use footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,16 @@
 import { Container } from "react-bootstrap";
 import styles from "./styles.module.css";
-import Header from "../components/common/Header/Header";
+import {Header} from "../components/common";
+import {Footer} from './../components/common';
 const { container, wrapper } = styles;
 const MainLayout = () => {
   return (
     <Container className={container}>
       <div className={wrapper}>
       <Header/>
+
       </div>
+      <Footer/>
     </Container>
   );
 };
