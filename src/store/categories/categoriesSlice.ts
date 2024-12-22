9c54e80d017ffc7b94a6ff06b8c29e5b172eef08
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 16:35:52 +0200

    add extra reducer to gte categories

--- src/store/categories/categoriesSlice.ts
+++ src/store/categories/categoriesSlice.ts
@@ -1,6 +1,8 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface ICategory {
+import actGetCategories from "./act/actGetCategories";
+
+export interface ICategory {
   id: number;
   title: string;
   prefix: TCategoryPrefix;
@@ -11,7 +13,7 @@ type TCategoryPrefix = "men" | "women" | "baby" | "sport";
 interface ICategoriesState {
   records: ICategory[];
   loading: "idle" | "pending" | "succeeded" | "failed";
-  error: null;
+  error: string | null;
 }
 const initialState: ICategoriesState = {
   records: [],
@@ -23,6 +25,23 @@ const categoriesSlice = createSlice({
   name: "categories",
   initialState,
   reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(actGetCategories.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    }),
+      builder.addCase(actGetCategories.fulfilled, (state, action) => {
+        state.loading = "succeeded";
+        state.error = null;
+        state.records = action.payload;
+      }),
+      builder.addCase(actGetCategories.rejected, (state, action) => {
+        state.loading = "failed";
+        // state.error = action.error.message || "";
+        state.error = action.payload as string;
+      });
+  },
 });
 
 export const {} = categoriesSlice.actions;
