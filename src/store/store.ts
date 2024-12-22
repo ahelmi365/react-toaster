d72b07b5bdec6c2a150b0df5293634248835b6a4
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 19:24:04 +0200

    export productsSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 // reducer
 
 import categoriesSlice from "./categories/categoriesSlice.ts";
+import productsSlice from "./products/productsSlice.ts";
 
 export const store = configureStore({
   reducer: {
     categories: categoriesSlice,
+    products: productsSlice,
   },
 });
 
