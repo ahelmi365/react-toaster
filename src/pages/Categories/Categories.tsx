e357ba476bba12a9a070d5d04c929e16ecdb5455
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 18:21:27 +0200

    useAppDisaptch

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,30 +1,45 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { Category } from "@components/eCommerce";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import actGetCategories from "@store/categories/act/actGetCategories";
+import { useEffect } from "react";
 const Categories = () => {
-  return (
-    <Container>
-      <Row className="justify-content-center g-2">
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center" >
-          <Category />
-        </Col>
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center" >
-          <Category />
-        </Col>
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center" >
-          <Category />
-        </Col>
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center" >
-          <Category />
-        </Col>
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center" >
-          <Category />
-        </Col>
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center" >
-          <Category />
-        </Col>
-      </Row>
-    </Container>
+  const dispatch = useAppDispatch();
+  const { loading, error, records } = useAppSelector(
+    (state) => state.categories
   );
+
+  useEffect(() => {
+    dispatch(actGetCategories());
+    console.log({ records });
+  }, []);
+
+  const renderedCategories = records?.map((category) => (
+    <Col
+      xs={12}
+      sm={6}
+      md={3}
+      className="d-flex justify-content-center"
+      key={category.id}
+    >
+      <Category {...category} />
+    </Col>
+  ));
+
+  // if (loading) {
+  //   return <p>Loading..</p>;
+  // }
+  // if (error) {
+  //   return <p>Error</p>;
+  // }
+
+  if (records) {
+    return (
+      <Container>
+        <Row className="justify-content-center g-2">{renderedCategories}</Row>
+      </Container>
+    );
+  }
 };
 
 export default Categories;
