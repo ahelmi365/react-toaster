f2bbf5365bca46709ac389c1f63002b096ff387a
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 19:23:43 +0200

    useappdispatch for products

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,29 +1,38 @@
 import { Product } from "@components/eCommerce";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import actGetProductsByCategoryPrefix from "@store/products/act/actGetProductsByCategoryPrefix";
+import { resetProductsRecords } from "@store/products/productsSlice";
+import { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const Products = () => {
+  const dispatch = useAppDispatch();
+  let { error, loading, records } = useAppSelector((state) => state.products);
+  const params = useParams();
+  const catPrefix = params.prefix;
+  useEffect(() => {
+    dispatch(actGetProductsByCategoryPrefix(catPrefix!));
+
+    return () => {
+      dispatch(resetProductsRecords());
+    };
+  }, [catPrefix]);
+
+  const renderedProductsList = records?.map((product) => (
+    <Col
+      xs={12}
+      sm={6}
+      md={3}
+      className="d-flex justify-content-center"
+      key={product.id}
+    >
+      <Product {...product} />
+    </Col>
+  ));
   return (
     <Container>
-      <Row className="justify-content-center g-2">
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center">
-          <Product />
-        </Col>
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center">
-          <Product />
-        </Col>
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center">
-          <Product />
-        </Col>
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center">
-          <Product />
-        </Col>
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center">
-          <Product />
-        </Col>
-        <Col xs={12} sm={6} md={3} className="d-flex justify-content-center">
-          <Product />
-        </Col>
-      </Row>
+      <Row className="justify-content-center g-2">{renderedProductsList}</Row>
     </Container>
   );
 };
