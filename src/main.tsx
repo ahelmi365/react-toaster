20dcb742ca73a4bb851c398a9521b366db8bf9e6
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 13:38:09 +0200

    add routes to the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,41 @@
 import { createRoot } from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// layout
 import MainLayout from "@layouts/MainLayout";
+// pages
+import Home from "@pages/Home/Home";
+import Categories from "@pages/Categories/Categories";
+import Products from "./pages/Products/Products";
+import AboutUs from "@pages/AboutUs/AboutUs";
 
 // css
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/categories",
+        element: <Categories />,
+      },
+      {
+        path: "/products/:prefix",
+        element: <Products />,
+      },
+      {
+        path: "/about-us",
+        element: <AboutUs />,
+      },
+    ],
+  },
+]);
 createRoot(document.getElementById("root")!).render(
-  <MainLayout/>
+  <RouterProvider router={router}></RouterProvider>
 );
