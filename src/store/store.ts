e99ec2ac21212efde8fcba79766f51a7a7c6e5eb
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 16:03:17 +0200

    wrap app with redux provider

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 // reducer
 
-import categoriesSlice from './categories/categoriesSlice.ts';
+import categoriesSlice from "./categories/categoriesSlice.ts";
 
 export const store = configureStore({
   reducer: {
     categories: categoriesSlice,
   },
-})
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
