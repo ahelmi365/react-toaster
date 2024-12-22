3ecb22440ccf1c3a2ceef2b31c4931be414caf49
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 16:21:57 +0200

    add proxies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+const serverDomain = "http://localhost:5005";
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/categories": {
+        target: `${serverDomain}`,
+        changeOrigin: true,
+      },
+      "/products": {
+        target: `${serverDomain}`,
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react(), tsconfigPaths()],
 });
