2bed9224c398ea4bba1695396654016ffe4c202f
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 13:53:13 +0200

    add global css

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import AppRouter from "@routes/AppRouter";
 
 // css
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@styles/global.css"
 
 createRoot(document.getElementById("root")!).render(<AppRouter />);
