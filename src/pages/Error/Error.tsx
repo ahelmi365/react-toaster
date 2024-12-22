4a9789fc975a5af33b88982debaa394f9705aec4
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 14:06:33 +0200

    add style to not found page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -2,11 +2,11 @@ import { Container } from "react-bootstrap"
 import { Link } from "react-router-dom"
 const Error = () => {
   return (
-    <Container>
+    <Container className="notFound">
         <h1>404</h1>
         <p>Page Not Found!</p>
 
-        <Link to="/" replace={true}>Go Home</Link>
+        <Link to="/" replace={true}>Go Home 🏠</Link>
     </Container>
   )
 }
