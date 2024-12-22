7fa49c137dfada3c9b942b846164034abf2b851f
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 10:03:32 +0200

    import bootstrap css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
+import MainLayout from "./layouts/MainLayout";
 
+// css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById("root")!).render(
-  <h1>Hello </h1>
+  <MainLayout/>
 );
