6796e87b1ce2506b6d0b179fd7b1863a60632f22
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 11:47:26 +0200

    update ordering of sections in layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,16 +1,14 @@
 import { Container } from "react-bootstrap";
 import styles from "./styles.module.css";
-import {Header} from "../components/common";
-import {Footer} from './../components/common';
+import { Header } from "../components/common";
+import { Footer } from "./../components/common";
 const { container, wrapper } = styles;
 const MainLayout = () => {
   return (
     <Container className={container}>
-      <div className={wrapper}>
-      <Header/>
-
-      </div>
-      <Footer/>
+      <Header />
+      <div className={wrapper}></div>
+      <Footer />
     </Container>
   );
 };
