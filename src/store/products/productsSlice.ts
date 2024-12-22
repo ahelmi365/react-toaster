7fab670a0c2ec15ca59b6115f8118282bf8ad700
Ali Helmi <ahelmi365@gmail.com>
2024-12-22 19:24:15 +0200

    init productsSlice

---
+++ src/store/products/productsSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+import { TLoading } from "@customTypes/shared";
+
+import { IProduct } from "./../../types/product";
+import actGetProductsByCategoryPrefix from "./act/actGetProductsByCategoryPrefix";
+interface IProductsState {
+  records: IProduct[];
+  loading: TLoading;
+  error: string | null;
+}
+
+const initialState: IProductsState = {
+  records: [],
+  loading: "idle",
+  error: null,
+};
+
+const productsSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {
+    resetProductsRecords: (state) => {
+      state.records = [];
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(actGetProductsByCategoryPrefix.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    }),
+      builder.addCase(
+        actGetProductsByCategoryPrefix.fulfilled,
+        (state, action) => {
+          state.loading = "succeeded";
+          state.error = null;
+          state.records = action.payload;
+        }
+      ),
+      builder.addCase(
+        actGetProductsByCategoryPrefix.rejected,
+        (state, action) => {
+          state.loading = "failed";
+          // state.error = action.error.message || "";
+          state.error = action.payload as string;
+        }
+      );
+  },
+});
+
+export const {resetProductsRecords} = productsSlice.actions;
+export default productsSlice.reducer;
