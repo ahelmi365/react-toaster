842e30ded358da2878d8f58238a9f0067ead50bf
Ali Helmi <ahelmi365@gmail.com>
2024-12-23 09:16:39 +0200

    use loading in products

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,14 +1,15 @@
-import { Product } from "@components/eCommerce";
-import { useAppDispatch, useAppSelector } from "@store/hooks";
 import actGetProductsByCategoryPrefix from "@store/products/act/actGetProductsByCategoryPrefix";
 import { resetProductsRecords } from "@store/products/productsSlice";
-import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { Col, Container, Row } from "react-bootstrap";
+import { Product } from "@components/eCommerce";
+import { Loading } from "@components/feedback";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 const Products = () => {
   const dispatch = useAppDispatch();
-  let { error, loading, records } = useAppSelector((state) => state.products);
+  const { error, loading, records } = useAppSelector((state) => state.products);
   const params = useParams();
   const catPrefix = params.prefix;
   useEffect(() => {
@@ -32,7 +33,9 @@ const Products = () => {
   ));
   return (
     <Container>
-      <Row className="justify-content-center g-2">{renderedProductsList}</Row>
+      <Loading status={loading} error={error}>
+        <Row className="justify-content-center g-2">{renderedProductsList}</Row>
+      </Loading>
     </Container>
   );
 };
