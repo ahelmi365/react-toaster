47f4c1bdf5880b8ebc0677e5e2522311e47f1c8f
Ali Helmi <ahelmi365@gmail.com>
2024-12-23 10:28:01 +0200

    remvoe old static grid

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,9 +1,9 @@
 import actGetCategories from "@store/categories/act/actGetCategories";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
-import { Col, Container, Row } from "react-bootstrap";
 import { Category } from "@components/eCommerce";
 import { Loading } from "@components/feedback";
 import { GridList } from "@components/common";
+import { Container } from "react-bootstrap";
 import { useEffect } from "react";
 
 const Categories = () => {
@@ -18,23 +18,10 @@ const Categories = () => {
     }
   }, [records]);
 
-  const renderedCategories = records?.map((category) => (
-    <Col
-      xs={12}
-      sm={6}
-      md={3}
-      className="d-flex justify-content-center"
-      key={category.id}
-    >
-      <Category {...category} />
-    </Col>
-  ));
-
   if (records) {
     return (
       <Container>
         <Loading status={loading} error={error}>
-          <Row className="justify-content-center g-2">{renderedCategories}</Row>
           <GridList
             records={records}
             renderItem={(category) => <Category {...category} />}
