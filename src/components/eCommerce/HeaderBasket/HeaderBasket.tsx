7b00374ee45508e0e3e8aab4062aaae285cac492
Ali Helmi <ahelmi365@gmail.com>
2024-12-23 15:32:52 +0200

    feat: add animation to basket quantity display on item change

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,19 +1,36 @@
 import { selectTotalNumberOfItemsInCart } from "@store/cart/selectors";
 import { BasketLogo } from "@components/common";
 import { useAppSelector } from "@store/hooks";
+import { useEffect, useState } from "react";
 
 import styles from "./styles.module.css";
 
-const { basketContainer, basketQuantity } = styles;
+const { basketContainer, basketQuantity, animatedQuantity } = styles;
 
 const HeaderBasket = () => {
+  const [isAnimated, setIsAnimated] = useState(false);
+
   const totalNumberOfItemsInCart = useAppSelector(
     selectTotalNumberOfItemsInCart
   );
+
+  useEffect(() => {
+    if (totalNumberOfItemsInCart === 0) return;
+    setIsAnimated(true);
+    const timeoutId = setTimeout(() => {
+      setIsAnimated(false);
+    }, 300);
+
+    return () => clearTimeout(timeoutId);
+  }, [totalNumberOfItemsInCart]);
   return (
     <div className={basketContainer}>
       <BasketLogo />
-      <div className={basketQuantity}>{totalNumberOfItemsInCart}</div>
+      <div
+        className={`${basketQuantity} ${isAnimated ? animatedQuantity : ""} `}
+      >
+        {totalNumberOfItemsInCart}
+      </div>
     </div>
   );
 };
