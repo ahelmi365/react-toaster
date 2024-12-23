a8c6842c0e59373c64352561fd38e78e9a471f0a
Ali Helmi <ahelmi365@gmail.com>
2024-12-23 09:16:31 +0200

    disable proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-const serverDomain = "http://localhost:5005";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// const serverDomain = "http://localhost:5005";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-
-    proxy: {
-      "/xcategories": {
-        target: `${serverDomain}`,
-        changeOrigin: true,
-      },
-      "/products": {
-        target: `${serverDomain}`,
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   "/xcategories": {
+    //     target: `${serverDomain}`,
+    //     changeOrigin: true,
+    //   },
+    //   "/products": {
+    //     target: `${serverDomain}`,
+    //     changeOrigin: true,
+    //   },
+    // },
   },
   plugins: [react(), tsconfigPaths()],
 });
